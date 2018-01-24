f70cdcec06f47c40241da4e0ed1422e8d037ec38
Sydney Larson <larsons1@rose-hulman.edu>
2018-01-24 11:30:13 -0500

    Finished to dos

--- src/m6_mutation.py
+++ src/m6_mutation.py
@@ -4,7 +4,7 @@ In this module, you mutate by CHANGING elements of a list.
 
 Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
          Claude Anderson, Delvin Defoe, Curt Clifton, their colleagues,
-         and PUT_YOUR_NAME_HERE.
+         and Sydney Larson.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # ----------------------------------------------------------------------
@@ -129,6 +129,13 @@ def RETURN_replace_negatives_by_zeros(numbers):
 
     Precondition: The argument is a list of numbers.
     """
+    new = []
+    for k in range(len(numbers)):
+        if numbers[k] > 0:
+            new = new + [numbers[k]]
+        else:
+            new = new + [0]
+    return new
     # TODO: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
@@ -185,6 +192,9 @@ def MUTATE_replace_negatives_by_zeros(numbers):
 
     Precondition: The argument is a list of numbers.
     """
+    for k in range(len(numbers)):
+        if numbers[k] < 0:
+            numbers[k] = 0
     # TODO: 3. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
